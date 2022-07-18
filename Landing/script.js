68ced80bf37992ad534ddf9e5994999f22307384
nemcevaanna <90156836+nemcevaanna@users.noreply.github.com>
2022-07-19 01:30:31 +0300

    Add files via upload

---
+++ Landing/script.js
@@ -0,0 +1,17 @@
+// alert('hi')
+
+// confirm('hi')
+
+// prompt('hi')
+
+// age = prompt("Ваш возраст")
+// if(age > 18){
+//     alert('Добро пожаловать')
+// } else{
+//     alert('Вы молоды...')
+// }
+
+
+let title = document.getElementsByClassName('van-bg-text')
+
+console.log(title)
